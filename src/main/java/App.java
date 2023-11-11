7739747dfba096d43b38c869bea2e4a4912c48f9
Léo Vernicos <leo.vernicos@etu.u-paris.fr>
2023-11-11 17:42:05 +0100

    Test random de Swing & logoTitle pas fini

---
+++ src/main/java/App.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import javax.swing.*;
+
+// Main class
+
+class App {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame(); // creating instance of JFrame
+
+        frame.setSize(700, 700); // 400 width and 500 height
+        frame.setLayout(null); // using no layout managers
+        frame.setVisible(true); // making the frame visible
+    }
+}
