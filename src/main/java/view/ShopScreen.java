b799d996ce054186e3d2dc1f6422ddc5afad6eee
AmaelMSC <amael.mangeantsastre@gmail.com>
2024-01-10 17:26:39 +0100

    Added exit button to the shop ;)

--- src/main/java/view/ShopScreen.java
+++ src/main/java/view/ShopScreen.java
@@ -22,6 +22,7 @@ import static tools.MathTools.nearestUnitPoint;
 
 public class ShopScreen extends JPanel {
     private static GameScreen gameScreen;
+    private static boolean errorDisplaying = false;
     private static final ArrayList<SpriteImage> sprites = new ArrayList<>();
     public ShopScreen(Game game, GameScreen gameScreen) throws IOException {
         ShopScreen.gameScreen = gameScreen;
@@ -31,37 +32,54 @@ public class ShopScreen extends JPanel {
 
         JButton buyOakButton = new JButton("Buy Oak");
         buyOakButton.addActionListener(e -> {
-            //Si on a assez d'argent
-            if(Game.getPlayerMoney().getAmount() >= Oak.cost){
+            //Si une erreur n'est pas en train de s'afficher
+            if(!errorDisplaying){
 
-                setVisible(false);
-                gameScreen.showAllGameScreen();
-                GameScreen.placingATree = true;
+                //Si on a assez d'argent
+                if(Game.getPlayerMoney().getAmount() >= Oak.cost){
 
-                try {
-                    gameScreen.addSpriteToMouseCursor(new SpriteImage(ImageIO.read(new File("src/main/resources/treedef.png")), 0,0,"Oak", new Oak(0,0, game.getMap())),0,0);
-                } catch (IOException ex) {
-                    throw new RuntimeException(ex);
-                }
+                    setVisible(false);
+                    gameScreen.showAllGameScreen();
+                    GameScreen.placingATree = true;
 
-                Game.getPlayerMoney().remove(new Money(Oak.cost));
-            }
-            //Si on a pas assez d'argent
-            else{
-                //Affiche pendant 3 secondes le message d'erreur
-                errorMessageLabel.setText("Error : Not Enough Money");
-                Timer timer = new Timer(3000, new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        errorMessageLabel.setText("");
+                    try {
+                        gameScreen.addSpriteToMouseCursor(new SpriteImage(ImageIO.read(new File("src/main/resources/treedef.png")), 0,0,"Oak", new Oak(0,0, game.getMap())),0,0);
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
                     }
-                });
-                timer.setRepeats(false);
-                timer.start();
+
+                    Game.getPlayerMoney().remove(new Money(Oak.cost));
+                }
+                //Si on a pas assez d'argent
+                else{
+                    //Affiche pendant 3 secondes le message d'erreur
+                    errorDisplaying = true;
+                    errorMessageLabel.setText("Error : Not Enough Money");
+                    Timer timer = new Timer(3000, new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            errorMessageLabel.setText("");
+                            errorDisplaying = false;
+                        }
+                    });
+                    timer.setRepeats(false);
+                    timer.start();
+                }
             }
         });
 
+
+        JButton exitButton = new JButton("Exit");
+        exitButton.addActionListener(e -> {
+            setVisible(false);
+            GameScreen.getMainContainer().setVisible(true);
+            GameScreen.getSideMenu().setVisible(true);
+            gameScreen.playGame();
+        });
+
         this.add(buyOakButton);
+
+        this.add(exitButton);
 
         this.setLayout(new FlowLayout());
 
